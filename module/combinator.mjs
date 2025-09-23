0f30286f61034ff3d31d9defa6b544f4c91c3cc6
dh10110 <dh10110@gmail.com>
2025-09-23 08:46:23 -0400

    UI Experiment

---
+++ module/combinator.mjs
@@ -0,0 +1,8 @@
+
+console.log('Module combinator loaded');
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM fully loaded and parsed');
+});
+
+console.log('Module combinator end');
